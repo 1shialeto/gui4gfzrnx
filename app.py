4904f95e2d870218812abc8b5e47a8391bca53e5
1shialeto <artemusolcev4@gmail.com>
2024-02-08 15:35:42 +0700

    initial commit

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import tkinter as tk
 import subprocess
 from tkinter import scrolledtext
 
+
 def run_console_app():
-    process = subprocess.Popen('gfzrnx_2.1.7_win64.exe -h', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    process = subprocess.Popen('gfzrnx_2.1.7_win64.exe -h', stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                               shell=True)
     output, _ = process.communicate()
     output_text.insert(tk.END, output.decode('utf-8'))
+
 
 app = tk.Tk()
 app.title("Console App Runner")
